44c7c322f16e0219f03cac8c0d480f766dc15a70
Congee <congeec@qq.com>
2022-08-18 21:30:09 -0400

    feat: add fmt::println

--- src/common.hpp
+++ src/common.hpp
@@ -1,6 +1,7 @@
 #include <optional>
 
 #include <fmt/format.h>
+#include <fmt/os.h>
 #include <tl/expected.hpp>
 
 #define Some std::make_optional
@@ -8,3 +9,14 @@
 
 #define Ok(x) (x)
 #define Err tl::make_unexpected
+
+namespace fmt {
+
+// println is missing https://github.com/fmtlib/fmt/issues/428
+template <typename... Args>
+void println(const Args&... args) {
+  fmt::print(args...);
+  std::putc('\n', stdout);
+}
+
+}  // namespace fmt
