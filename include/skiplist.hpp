fc3e70e1ce029e851eb3f6134e3abd0a36527f36
Congee <congeec@qq.com>
2022-08-20 18:05:46 -0400

    feat: a skiplist implementation for MemTable
    close #5

--- include/skiplist.hpp
+++ include/skiplist.hpp
@@ -1,12 +1,35 @@
 #include <optional>
 #include <string>
+#include <vector>
 
 using Key = std::string;
 using Value = std::string;
 
+struct Node {
+  Key key;
+  Value value;
+
+  std::vector<Node*> forward;
+  Node(const Key& k, const Value& v, int level) noexcept
+    : key(k), value(v), forward(std::vector<Node*>(level + 1, nullptr)) {}
+};
+
+/// Skiplist is not thread-safe yet
 class Skiplist {
+  /// A dummy node before the 1st  node. It's always the tallest node;
+  Node* head;
+  int level;
+  size_t __size;
+
+  Node* find(const Key& key) const noexcept;
+  std::optional<Value> pop(const Key& key) noexcept;
+
  public:
+  Skiplist() noexcept;
+
+  size_t size() const noexcept { return __size; }
+
   void put(const Key& key, const Value& value) noexcept;
   std::optional<Value> get(const Key& key) const noexcept;
-  void del(const Key& key) noexcept;
+  bool del(const Key& key) noexcept;
 };
--- tests/test_skiplist.cpp
+++ tests/test_skiplist.cpp
@@ -2,8 +2,31 @@
 
 #include "skiplist.hpp"
 
+TEST(Skiplist, skiplist_get_from_empty) {
+  Skiplist sl;
+  EXPECT_FALSE(sl.get("foo").has_value());
+}
+
 TEST(Skiplist, skiplist) {
   Skiplist sl;
-  sl.put("foo", "bar");
-  EXPECT_FALSE(sl.get("foo").has_value());
+  EXPECT_FALSE(sl.get("nonexistent").has_value());
+
+  sl.put("foo", "foo");
+  sl.put("bar", "bar");
+  sl.put("qux", "qux");
+
+  EXPECT_EQ(sl.get("foo").value(), "foo");
+  EXPECT_FALSE(sl.get("nonexistent").has_value());
+
+  sl.put("foo", "foo0");
+  EXPECT_EQ(sl.get("foo").value(), "foo0");
+
+  // clang-format off
+  sl.del("foo"); EXPECT_EQ(sl.size(), 2);
+  sl.del("bar"); EXPECT_EQ(sl.size(), 1);
+  sl.del("qux"); EXPECT_EQ(sl.size(), 0);
+  // clang-format on
+
+  EXPECT_FALSE(sl.del("foo"));
+  EXPECT_EQ(sl.size(), 0);
 }
